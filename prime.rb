b76eae1b0add81e15845b2d2b21e4bc30cf60e5d
IvanTorres <herox831@outlook.com>
2020-11-01 00:46:15 -0700

    commit tree 4b245a2c10aa557a9cda479c1abf3abaaba2c5a1

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+    first = 2
+    if num > 1
+        second = (first..num-1).to_a
+        second.none? do |num_test|
+            num % num_test == 0
+        end
+    else
+        false
+    end
+end
